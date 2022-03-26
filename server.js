98b4c16907ac8563a2035c4528ebcfb73dcc5089
Nathan Kenopic <nkenopic@myseneca.ca>
2022-03-26 11:19:25 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<bti425-a3-nathan-kenopic>'));
+app.use(express.static(__dirname + '/dist/bti425-a3-nathan-kenopic'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<bti425-a3-nathan-kenopic>/index.html'));});
+'/dist/bti425-a3-nathan-kenopic/index.html'));});
 app.listen(process.env.PORT || 8080);
